00ab3377dfeaf69b21e0170ac1bb0c17394960f7
Adlai M.B <adlaimb@gmail.com>
2021-10-06 11:22:52 -0700

    add main menu to the home screen

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimeLineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimeLineActivity.java
@@ -1,5 +1,6 @@
 package com.codepath.apps.restclienttemplate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -7,6 +8,9 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -64,6 +68,28 @@ public class TimeLineActivity extends AppCompatActivity {
         rvTweets.setAdapter(adapter);
 
         populateHomeTimeline();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.compose){
+
+            // Compose icon is tapped/clicked
+            Toast.makeText(this,"Compose",Toast.LENGTH_SHORT).show();
+            // Navigate to the compose activity
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateHomeTimeline() {
